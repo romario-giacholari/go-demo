f86ab540c0c4f8d4a2247c9e7b1da8e3dd6601cc
romario-giacholari <giacholari@gmail.com>
2022-09-21 13:34:01 +0100

    setup aup mod file and alter syntax (add commas)

--- app.go
+++ app.go
@@ -13,7 +13,7 @@ func (person *Person) getName() string {
 }
 
 func (person *Person) isOverAge() bool {
-	return person.age > 18
+	return person.age >= 18
 }
 
 func times (number int, times int) float32 {
@@ -21,32 +21,35 @@ func times (number int, times int) float32 {
 }
 
 func main() {
-	var person = Person { 
+	person := Person { 
 		name: "romario", 
-		age: 26 }
+		age: 26,
+	 }
 
 	fmt.Println(times(person.age, 10))
-    fmt.Println(person.name, person.age)
+	fmt.Println(person.name, person.age)
 	fmt.Println(person.getName())
 	fmt.Println(person.isOverAge())
 
-	var arr = [3]int{1, 2, 3}
+	arr := [3]int{1, 2, 3}
 	for i := 0; i < len(arr); i++ {
 		fmt.Println(arr[i])
 	}
 
 	// first element from arr
-	var flexibleSlice = arr[0:1]
+	flexibleSlice := arr[0:1]
 	fmt.Println(len(flexibleSlice))
 
-	var associative = map[string]string {
+	associative := map[string]string {
 		"foo": "bar",
-		"baz": "boo"}
+		"baz": "boo",
+	}
+	
 	for key, value := range associative {
         fmt.Println(key, value)
     }
 
-	var value, ok = associative["is"]
+	value, ok := associative["is"]
 	fmt.Printf("value:%v type:%T", value, value)
 	fmt.Println()
 	fmt.Printf("ok:%v type:%T", ok, ok)
---
+++ go.mod
@@ -0,0 +1,3 @@
+module example.com/go-demo
+
+go 1.18
